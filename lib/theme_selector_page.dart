d1cbe6d9c6092a4da7c303ba7bfbbe9a29b8caf0
Jorge Coca <jcocaramos@gmail.com>
2018-08-11 16:48:05 -0500

    Added themes BLOC, but still not switching themes

--- lib/theme_selector_page.dart
+++ lib/theme_selector_page.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:theme_switcher/themes.dart';
 
 class ThemeSelectorPage extends StatelessWidget {
+  final ThemeBloc themeBloc;
+
+  ThemeSelectorPage({Key key, this.themeBloc}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,9 +20,8 @@ class ThemeSelectorPage extends StatelessWidget {
           child: Column(
             children: <Widget>[
               RaisedButton(
-                onPressed: () {
-                  // do nothing for now
-                },
+                onPressed: () =>
+                    themeBloc.selectedTheme.add(_buildLightTheme()),
                 child: Text(
                   'Light theme',
                 ),
@@ -25,9 +29,8 @@ class ThemeSelectorPage extends StatelessWidget {
               Padding(
                 padding: const EdgeInsets.only(top: 8.0),
                 child: RaisedButton(
-                  onPressed: () {
-                    // do nothing for now
-                  },
+                  onPressed: () =>
+                      themeBloc.selectedTheme.add(_buildDarkTheme()),
                   child: Text(
                     'Dark theme',
                   ),
@@ -38,5 +41,25 @@ class ThemeSelectorPage extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  DemoTheme _buildLightTheme() {
+    return DemoTheme(
+        'light',
+        ThemeData(
+          brightness: Brightness.light,
+          accentColor: Colors.deepOrange,
+          primaryColor: Colors.cyanAccent,
+        ));
+  }
+
+  DemoTheme _buildDarkTheme() {
+    return DemoTheme(
+        'dark',
+        ThemeData(
+          brightness: Brightness.dark,
+          accentColor: Colors.lightBlueAccent,
+          primaryColor: Colors.pink,
+        ));
   }
 }
---
+++ lib/themes.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:rxdart/rxdart.dart';
+import 'dart:async';
+
+class DemoTheme {
+  final String name;
+  final ThemeData data;
+
+  const DemoTheme(this.name, this.data);
+}
+
+class ThemeBloc {
+  final Stream<ThemeData> themeDataStream;
+  final Sink<DemoTheme> selectedTheme;
+
+  factory ThemeBloc() {
+    final selectedTheme = PublishSubject<DemoTheme>();
+    final themeDataStream = selectedTheme
+        .distinct()
+        .map((theme) => theme.data);
+    return ThemeBloc._(themeDataStream, selectedTheme);
+  }
+
+  const ThemeBloc._(this.themeDataStream, this.selectedTheme);
+}
