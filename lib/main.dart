0eac31afbc886c891f75ddecc5f9d100ad6e82e9
Jorge Coca <jorge.coca@bmwna.com>
2018-08-13 07:35:24 -0500

    Removed StatefulWidget from the ThemeSwitcherApp

--- lib/main.dart
+++ lib/main.dart
@@ -4,31 +4,19 @@ import 'package:theme_switcher/themes.dart';
 
 void main() => runApp(new ThemeSwitcherApp());
 
-class ThemeSwitcherApp extends StatefulWidget {
-  @override
-  _ThemeSwitcherAppState createState() => _ThemeSwitcherAppState();
-}
-
-class _ThemeSwitcherAppState extends State<ThemeSwitcherApp> {
-  ThemeBloc _themeBloc;
-
-  @override
-  void initState() {
-    super.initState();
-    _themeBloc = ThemeBloc();
-  }
-
+class ThemeSwitcherApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final ThemeBloc themeBloc = ThemeBloc();
     return StreamBuilder<ThemeData>(
-      initialData: _themeBloc.initialTheme().data,
-      stream: _themeBloc.themeDataStream,
+      initialData: themeBloc.initialTheme().data,
+      stream: themeBloc.themeDataStream,
       builder: (BuildContext context, AsyncSnapshot<ThemeData> snapshot) {
         return MaterialApp(
           title: 'Theme Switcher',
           theme: snapshot.data,
           home: HomePage(
-            themeBloc: _themeBloc,
+            themeBloc: themeBloc,
           ),
         );
       },
